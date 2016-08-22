8c2e9c0e63b2250d3c58903dc58f9d9fc72aa35c
larsjoR <larsjo29@gmail.com>
2016-08-22 18:29:37 +0200

    Added Git module

---
+++ git/git.psm1
@@ -0,0 +1,18 @@
+﻿Function Push-Git {
+ Param(
+ [CmdletBinding()]
+    [Parameter(Mandatory = $true)]
+    [String]$Dir,
+    [Parameter(Mandatory = $true)]
+    [String]$Message
+)
+
+cd $Dir 
+git add *
+git commit -m $Message
+git push -u origin master
+
+}
+
+
+Export-ModuleMember -Function Push-Git
